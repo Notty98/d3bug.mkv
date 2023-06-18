562e2ca84b5f4b116ff6c962beeea331fee1899a
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-06-18 16:11:56 +0200

    frontend: added upload view

--- frontend/src/components/MapComponent.vue
+++ frontend/src/components/MapComponent.vue
@@ -6,16 +6,29 @@
     import View from 'ol/View'
     import Map from 'ol/Map'
     import TileLayer from 'ol/layer/Tile'
-    import OSM from 'ol/source/OSM'
+    import { OSM, Vector as VectorSource } from 'ol/source'
+    import { Vector as VectorLayer } from 'ol/layer'
 
     import 'ol/ol.css'
+    import Feature from 'ol/Feature'
+    import { Point } from 'ol/geom'
+    import { toLonLat } from 'ol/proj'
 
     export default {
         name: 'MapContainer',
         components: {},
-        props: {},
+        props: { 
+            latitude: {
+                type: Number,
+                default: null
+            },
+            longitude: {
+                type: Number,
+                default: null
+            }
+        },
         mounted() {
-            new Map({
+            const map = new Map({
                 target: this.$refs['map-root'],
                 layers: [
                     new TileLayer({
@@ -27,6 +40,30 @@
                     center: [0, 0],
                     constrainResolution: true
                 })
+            })
+
+            // handle click
+            const markerLayer = new VectorLayer({
+                source: new VectorSource(),
+            })
+            map.addLayer(markerLayer)
+
+            map.on('click', (e) => {
+                const markerSource = markerLayer.getSource()
+                const coordinate = e.coordinate
+                const latlon = toLonLat(coordinate)
+                console.log(latlon)
+
+                const latitude = latlon[1]
+                const longitude = latlon[0]
+
+                this.$emit('update:latitude', latitude)
+                this.$emit('update:longitude', longitude)
+
+                const markerFeature = new Feature({
+                    geometry: new Point(coordinate)
+                })
+                markerSource.addFeature(markerFeature)
             })
         }
     }
--- frontend/src/views/UploadPage.vue
+++ frontend/src/views/UploadPage.vue
@@ -2,20 +2,57 @@
     <!--<img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <NavBar></NavBar>
-    <div>
-        <h1>Upload</h1>
-        <input type="file" placeholder="select a folder" webkitdirectory multiple @change="event => handleFiles(event)" accept="image/png, image/jpeg, image/jpg"/>
-        <input type="file" placeholder="select a image" @change="event => handleFile(event)" accept="image/png, image/jpeg, image/jpg"/>
+    <div v-if="selectedCollection == null">
+        <h1>Select an existing collection</h1>
+        <b-dropdown text="Available collections">
+            <b-dropdown-item v-for="item in collections" :value="item.collection_id" :key="item.collection_id" @click="event => updateSelection(event, item)">{{ item.collection_name }}</b-dropdown-item>
+        </b-dropdown>
+        <!--<b-button variant="success" @click="event => handle(event)">Create Collection</b-button>-->
+        <h1>Create a new collection</h1>
+        <b-form-group
+            id="collection-group-name"
+            label="Collection Name"
+            label-for="collection-name">
+            <b-form-input
+                id="collection-name"
+                type="text"
+                placeholder="Enter collection name"
+                v-model="collection_name"
+                required
+            ></b-form-input>
+        </b-form-group>
+        <b-form-group
+            id="collection-group-desc"
+            label="Collection Description"
+            label-for="collection-desc">
+            <b-form-input
+                id="collection-desc"
+                type="text"
+                placeholder="Enter collection description"
+                v-model="collection_desc"
+            ></b-form-input>
+        </b-form-group>
+        <b-button variant="success" @click="event => handleCreationOfCollection(event)">Continue</b-button>
+    </div>
+    <div v-if="files.length == 0 && selectedCollection != null">
+        <h1>Select the file(s) to upload</h1>
+        <input type="file" @change="event => handleFiles(event)" accept="image/png, image/jpeg, image/jpg" webkitdirectory multiple />
+        <input type="file" @change="event => handleFiles(event)" accept="image/png, image/jpeg, image/jpg" />
+    </div>
+    <div v-if="files.length > 0 && selectedCollection != null" style="height: 100%;">
+        <h1>Choose the point on the map for the {{ this.files[index].name }} image</h1>
         <div style="height: 50%;">
-            <MapComponent></MapComponent>
+            <MapComponent v-on:update:latitude="updateLatitude" v-on:update:longitude="updateLongitude"></MapComponent>
         </div>
-  </div>
+        <b-button variant="success" @click="event => handleContinueButton(event)">Continue</b-button>
+    </div>
 </template>
   
 <script>
 
 import NavBar from '../components/NavBar.vue'
 import MapComponent from '../components/MapComponent.vue'
+import { getCollections, createCollection, addPhotoToCollection } from '../services/services'
   
 export default {
     name: 'UploadPage',
@@ -23,15 +60,79 @@ export default {
         NavBar,
         MapComponent
     },
+    data() {
+        return {
+            latitude: null,
+            longitude: null,
+            files: [],
+            //formData: new FormData(),
+            formData: [],
+            index: 0,
+            collections: [],
+            selectedCollection: null,
+            collection_name: null,
+            collection_desc: null
+        }
+    },
+    mounted() {
+        getCollections().then(data => { 
+            this.collections = data.data
+        })
+    },
     methods: {
+        updateLatitude(latitude) {
+            this.latitude = latitude
+            console.log(this.selectedCollection)
+        },
+        updateLongitude(longitude) {
+            this.longitude = longitude
+        },
         handleFiles(event) {
             const files = event.target.files
             console.log('handling files...')
-            console.log(files)
+            this.files = files
         },
-        handleFile(event) {
-            console.log('handling single file...')
+        handleContinueButton(event) {
+            console.log('click on continue')
             console.log(event)
+
+            const formData = new FormData()
+            formData.append('photo', this.files[this.index])
+            formData.append('lat', this.latitude)
+            formData.append('lon', this.longitude)
+
+            this.formData.push(formData)
+
+            if(this.index + 1 < this.files.length)
+                this.index = this.index + 1
+            else
+                this.handleUpload()
+        },
+        updateSelection(event, item) {
+            console.log('here')
+            this.selectedCollection = item.collection_id
+            console.log(this.selectedCollection)
+        },
+        handleCreationOfCollection(event) {
+            console.log(event)
+            this.selectedCollection = -1
+        },
+        async handleUpload() {
+            for(let form of this.formData) {
+                if(this.selectedCollection == -1) {
+                    // create a new collections
+                    console.log('create new collection')
+                    form.append('name', this.collection_name)
+                    form.append('desc', this.collection_desc)
+                    const response = await createCollection(form)
+                    this.selectedCollection = response.collectionId
+                    continue
+                }
+                // add photo to collection
+                addPhotoToCollection(form, this.selectedCollection)
+            }
+            console.log('done')
+            this.$router.push('/')
         }
     }
 }
