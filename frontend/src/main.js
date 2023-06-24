ebd9d0b7f5e80e3e5a64ebf67930924baf0c7060
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-06-24 09:41:26 +0200

    removed store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,10 +7,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import router from './router'
-import store from './store/store'
 
 const app = createApp(App)
 app.use(BootstrapVueNext)
 app.use(router)
-app.use(store)
 app.mount('#app')
