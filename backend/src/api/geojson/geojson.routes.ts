9af8453af7989165ffaef90650f67ddfae7bf91a
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-07-07 13:46:19 +0200

    added delete methods for geojson collection

--- backend/src/api/geojson/geojson.routes.ts
+++ backend/src/api/geojson/geojson.routes.ts
@@ -1,10 +1,13 @@
 import express from 'express'
-import { addGeoJson, getAllGeoJson } from './geojson.controller'
+import { addGeoJson, getAllGeoJson, removeGeoJsonById, removeAllGeoJsons } from './geojson.controller'
 import { geoJsonUpload } from '../upload/multer'
 
 const router = express.Router()
 
 router.post('/', geoJsonUpload.single(process.env.MULTER_FIELD_NAME_GEOJSON!), addGeoJson)
 router.get('/', getAllGeoJson)
+
+router.delete('/:id', removeGeoJsonById)
+router.delete('/', removeAllGeoJsons)
 
 export { router }
