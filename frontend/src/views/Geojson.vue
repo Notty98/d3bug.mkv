4fa131603e7e18985cfe7eec8cde5a8c8d9a7ae2
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-07-07 13:47:36 +0200

    updated the interface with the button to remove geojson

--- frontend/src/views/Geojson.vue
+++ frontend/src/views/Geojson.vue
@@ -2,14 +2,30 @@
     <!--<img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <NavBar></NavBar>
-    <h1>Geojson handler!</h1>
-    <input type="file" @change="event => handleFiles(event)" />
+    <div>
+        <h1>Upload a geojson</h1>
+        <input type="file" @change="event => handleFiles(event)" />
+    </div>
+
+    <div>
+        <h1>Remove an uploaded geojson</h1>
+        <select v-model="selectedGeoJson">
+            <option disabled selected value> -- select an option -- </option>
+            <option v-for="item in geojsons" :value="item.geojson_id" :key="item.geojson_id">{{ item.geojson_name }}</option>
+        </select>
+        <button @click="event => handleRemoveSelectedGeojson(event)">Remove the selected geojson</button>
+    </div>
+    <div>
+        <h1>Remove all uploaded geojsons</h1>
+        <button @click="event => handleRemoveAllGeojson(event)">Remove all</button>
+    </div>
+    
 </template>
   
 <script>
 
 import NavBar from '../components/NavBar.vue'
-import { addGeoJson } from '../services/services'
+import { addGeoJson, getGeoJsons, removeGeojsonById, removeAllGeoJsons } from '../services/services'
   
 export default {
     name: 'GeoJson',
@@ -18,11 +34,18 @@ export default {
     },
     data() {
         return {
-            formData: null
+            formData: null,
+            geojsons: [],
+            selectedGeoJson: null
         }
     },
     mounted() {
-        console.log(this.geojson)
+        getGeoJsons()
+            .then(data => {
+                for(const item of data.data) {
+                    this.geojsons.push(item)
+                }
+            })
     },
     methods: {
         async handleFiles(event) {
@@ -34,6 +57,22 @@ export default {
             await addGeoJson(this.formData)
             //this.addGeoJson(geojson)
             this.$router.push({ name: 'Home' });
+        },
+        handleRemoveSelectedGeojson(event) {
+            console.log(event)
+            removeGeojsonById(this.selectedGeoJson)
+                .then(data => {
+                    console.log(data)
+                    window.location.reload()
+                })
+        },
+        handleRemoveAllGeojson(event) {
+            console.log(event)
+            removeAllGeoJsons()
+                .then(data => {
+                    console.log(data)
+                    window.location.reload()
+                })
         }
     }
 }
