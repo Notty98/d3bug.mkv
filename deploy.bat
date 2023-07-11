e54457acb9326b6b52843e1765c38a3b9a489892
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-07-11 21:13:20 +0200

    updated deploy file

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
+::minikube start
 kubectl apply -f .\db-persistent-volume.yaml
 kubectl apply -f .\db-persistent-volume-claim.yaml
 kubectl apply -f .\db-configmap.yaml
@@ -5,6 +6,13 @@ kubectl apply -f .\db-deployment.yaml
 kubectl apply -f .\db-service.yaml
 kubectl apply -f .\backend-deployment.yaml
 kubectl apply -f .\backend-service.yaml
-timeout /t 60 /nobreak
+kubectl apply -f .\frontend-deployment.yaml
+kubectl apply -f .\frontend-service.yaml
+kubectl apply -f .\nginx-configmap.yaml
+kubectl apply -f .\nginx-deployment.yaml
+kubectl apply -f .\nginx-service.yaml
+::timeout /t 60 /nobreak
 ::kubectl port-forward deployment/postgresdb 5433:5432
-::kubectl port-forward deployment/backend 8081:8080
+::kubectl port-forward deployment/backend 9090:9090
+::kubectl port-forward deployment/frontend 8080:8080
+::kubectl port-forward deployment/nginx 80:80
